221efe26b38907fc001df71bff071f665fb3925d
Samuel <136899093+Samue45@users.noreply.github.com>
2024-06-03 10:36:29 +0200

    Refactor del Backend

--- Backend/api/HabitTracker/src/main/java/com/HabitTracker/HabitTracker/Auth/AuthController.java
+++ Backend/api/HabitTracker/src/main/java/com/HabitTracker/HabitTracker/Auth/AuthController.java
@@ -2,15 +2,13 @@ package com.HabitTracker.HabitTracker.Auth;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+// Controlador REST para la autenticación
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -18,17 +16,20 @@ public class AuthController {
 
     private final AuthService authService;
 
+    // Permitir solicitudes CORS desde el puerto 4200 (por ejemplo, una aplicación Angular en desarrollo)
     @CrossOrigin(origins = "http://localhost:4200")
-    @PostMapping("login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request)
-    {
-        return ResponseEntity.ok(authService.login(request));
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        // Delegar la lógica de autenticación al servicio de autenticación
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
     }
 
-    @PostMapping("register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) 
-    {
-        return ResponseEntity.ok(authService.register(request));
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        // Delegar la lógica de registro al servicio de autenticación
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
     }
-    
 }
+
---
+++ Backend/api/HabitTracker/src/main/java/com/HabitTracker/HabitTracker/Exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.HabitTracker.HabitTracker.Exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
